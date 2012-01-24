280c6f6000b09e0327d6559fe20ff05657b30565
Karen Kratyan <kratkar@yandex.ru>
2012-01-25 01:29:15 +0400

    Add play action

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,43 @@
 x$(document).on('deviceready', function () {
-  x$('#record_start').on('touchstart', function () {
-    x$('#record_log').html('Start recording(10 sec. maximum)')
-    console.log('Record start')
-    var file = '/record_1.mp3';
-    var m = new Media(file, function () {
-          console.log('record complete')
-          x$('#record_log').html('Record complete')
-        }, function () {
-          console.log('record error')
-          x$('#record_log').html('Record error')
-    })
+  var file = '/record1.mp3';
+  var m = new Media(file, function () {
+        //console.log('record complete')
+        x$('#recordLog').html('Record complete')
+        x$('#playActions').css({ 'display' : 'block' })
+      }, function () {
+        //console.log('record error')
+        x$('#recordLog').html('Record error')
+        x$('#playActions').css({ 'display' : 'none' })
+  })
+  x$('#recordStart').on('touchstart', function () {
+    x$('#recordLog').html('Start recording(10 sec. maximum)')
+    //console.log('Record start')
     m.startRecord();
-    console.log('m.startRecord', m)
+    //console.log('m.startRecord', m)
     setTimeout(function () {
-      console.log('stop over 10 sec.')
+      //console.log('stop over 10 sec.')
       m.stopRecording();
-      x$('#record_log').html('Record stop 10 sec max!')
+      x$('#recordLog').html('Record stop 10 sec max!')
     }, 10000)
   });
-  x$('#record_stop').on('touchstart', function () {
-      console.log('stop record')
-      m.stopRecording();
-      x$('#record_log').html('Record stop!')
+  x$('#recordStop').on('touchstart', function () {
+    //console.log('stop record')
+    m.stopRecording();
+    x$('#recordLog').html('Record stop!')
+  })
+  x$('#recordPlay').on('touchstart', function () {
+    var $t = x$(this), action = $t.attr('data-action');
+    if (action == 'play') {
+      console.log('play')
+      m.play()
+      $t.attr('data-action', 'stop')
+    }
+    else {
+      console.log('play')
+      m.stop()
+      $t.attr('data-action', 'play')
+    }
+    var textButton = $t.attr('data-text'+action)
+    x$(this).html(textButton);
   })
 }, false)
