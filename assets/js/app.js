bebb91c87e9048f22b6a7eb174dbff08cde22637
Karen <karen@kratkar.(none)>
2012-01-24 23:22:19 +0400

    display none

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 x$(document).on('deviceready', function () {
   x$('#tadam_show').on('touchstart', function () {
-    x$(this).css({ 'display' : 'block' })
+    x$('#tadam').css({ 'display' : 'block' })
   })
 }, false)
