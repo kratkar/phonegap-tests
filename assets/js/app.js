6da338f89c934ada476bcfebdc68cbd60e12bf75
Karen <karen@kratkar.(none)>
2012-01-25 00:14:46 +0400

    Error, button warning

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,18 @@
 x$(document).on('deviceready', function () {
   x$('#record_start').on('touchstart', function () {
-    x$('#record_message').html('Start recording(10 sec. maximum)').css({ 'display' : 'block' })
-    var file = '/record_1.mp3'
-    var m = new Media(file, function () {
-      x$('#record_log').html('Record complete')
-    }, function () {
-      x$('#record_log').html('Record failed')
-    })
+    x$('#record_log').html('Start recording(10 sec. maximum)')
+    var file = '/record_1.mp3',
+        m = new Media(file, function () {
+          x$('#record_log').html('Record complete')
+        }, function () {
+          x$('#record_log').html('Record failed')
+        })
     m.startRecord();
     setTimeout(function () {
       m.stopRecording();
       x$('#record_log').html('Record stop 10 sec max!')
     }, 10000)
-  })
+  });
   x$('#record_stop').on('touchstart', function () {
       m.stopRecording();
       x$('#record_log').html('Record stop!')
