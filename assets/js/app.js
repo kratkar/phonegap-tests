2426d7fb2d3ae20c6a7cd51d6fe98ac8244538b7
Karen <karen@kratkar.(none)>
2012-01-25 00:05:24 +0400

    Test Recording

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,20 @@
 x$(document).on('deviceready', function () {
-  x$('#tadam_show').on('touchstart', function () {
-    x$('#tadam').css({ 'display' : 'block' })
+  x$('#record_start').on('touchstart', function () {
+    x$('#record_message').html('Start recording(10 sec. maximum)').css({ 'display' : 'block' })
+    var file = '/record_1.mp3'
+    var m = new Media(file, function () {
+      x$('#record_log').html('Record complete')
+    }, function () {
+      x$('#record_log').html('Record failed')
+    })
+    m.startRecord();
+    setTimeout(function () {
+      m.stopRecording();
+      x$('#record_log').html('Record stop 10 sec max!')
+    }, 10000)
+  })
+  x$('#record_stop').on('touchstart', function () {
+      m.stopRecording();
+      x$('#record_log').html('Record stop!')
   })
 }, false)
