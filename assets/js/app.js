af05f497b1f9cedb5795c5b512a8de5f5ce0482b
Karen Kratyan <kratkar@yandex.ru>
2012-01-26 00:20:37 +0400

    Error: 10 sec, file log. Show action if undefinde textButton

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,26 @@
 x$(document).on('deviceready', function () {
 
   var file, timeoutId;
-  console.log('file = ' + file)
   var mediaObject = new Media(file, function () {
         //console.log('record complete')
         x$('#recordLog').html('record complete')
-        x$('#playActionFileName').html('File: ' + file)
-        x$('#playActions').css({ 'display' : 'block' })
       }, function () {
         //console.log('record error')
         x$('#recordLog').html('Record error')
-        x$('#playActions').css({ 'display' : 'none' })
   }), playObject;
 
   x$('#recordStart').on('touchstart', function () {
     file = x$('#recordFileName')[0].value
+    console.log('file = ' + file)
     x$('#recordLog').html('Start recording(10 sec. maximum)')
     //console.log('Record start')
     mediaObject.startRecord();
     //console.log('mediaObject.startRecord', m)
-    var timeoutId = setTimeout(function () {
+    timeoutId = setTimeout(function () {
       //console.log('stop over 10 sec.')
       mediaObject.stopRecord();
-      x$('#recordLog').html('Record stop 10 sec max!')
-    }, 1000)
+      x$('#recordLog').html('Record stop - 10 sec max!')
+    }, 10000)
   });
 
   x$('#recordStop').on('touchstart', function () {
@@ -36,7 +33,10 @@ x$(document).on('deviceready', function () {
   x$('#recordPlay').on('touchstart', function () {
     var $t = x$(this), action = $t.attr('data-action');
     playObject = new Media(file, function () {
+      x$('#playActionFileName').html('File: ' + file)
+      x$('#playActions').css({ 'display' : 'block' })
     }, function () {
+      x$('#playActions').css({ 'display' : 'none' })
     })
     if (action == 'play') {
       playObject.play()
@@ -48,7 +48,7 @@ x$(document).on('deviceready', function () {
     }
     console.log('action = '+action)
     var textButton = $t.attr('data-text'+action)
-    $t.html(textButton);
     console.log('textButton = ' + textButton)
+    $t.html(textButton?textButton:action);
   })
 }, false)
