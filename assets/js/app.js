4f61fde19db100ad200aae00bedf6cda015da768
Karen Kratyan <kratkar@yandex.ru>
2012-01-26 00:00:48 +0400

    error, css button, test text play|stop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,32 @@
 x$(document).on('deviceready', function () {
 
   var file = x$('#recordFileName')[0].value, timeoutId;
+  console.log('file = ' + file)
   var mediaObject = new Media(file, function () {
-        console.log('record complete')
+        //console.log('record complete')
         x$('#recordLog').html('record complete')
         x$('#playActionFileName').html('File: ' + file)
         x$('#playActions').css({ 'display' : 'block' })
       }, function () {
-        console.log('record error')
+        //console.log('record error')
         x$('#recordLog').html('Record error')
         x$('#playActions').css({ 'display' : 'none' })
   }), playObject;
 
   x$('#recordStart').on('touchstart', function () {
     x$('#recordLog').html('Start recording(10 sec. maximum)')
-    console.log('Record start')
+    //console.log('Record start')
     mediaObject.startRecord();
-    console.log('mediaObject.startRecord', m)
+    //console.log('mediaObject.startRecord', m)
     var timeoutId = setTimeout(function () {
       //console.log('stop over 10 sec.')
       mediaObject.stopRecord();
       x$('#recordLog').html('Record stop 10 sec max!')
-    }, 10000)
+    }, 1000)
   });
 
   x$('#recordStop').on('touchstart', function () {
-    console.log('stop record')
+    //console.log('stop record')
     mediaObject.stopRecord();
     clearTimeout(timeoutId);
     x$('#recordLog').html('Record stop!')
@@ -33,18 +34,20 @@ x$(document).on('deviceready', function () {
 
   x$('#recordPlay').on('touchstart', function () {
     var $t = x$(this), action = $t.attr('data-action');
+    playObject = new Media(file, function () {
+    }, function () {
+    })
     if (action == 'play') {
-      console.log('play')
-      mediaObject.play()
+      playObject.play()
       $t.attr('data-action', 'stop')
     }
     else {
-      console.log('stop')
-      mediaObject.stop()
+      playObject.stop()
       $t.attr('data-action', 'play')
     }
+    console.log('action = '+action)
     var textButton = $t.attr('data-text'+action)
     $t.html(textButton);
-    console.log(textButton)
+    console.log('textButton = ' + textButton)
   })
 }, false)
